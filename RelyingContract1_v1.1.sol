5ab3a05b35fbc581a9de57689cb2d85d7e7cc9e5
georgejohnchavady <46595150+georgejohnchavady@users.noreply.github.com>
2020-07-12 18:06:18 +0100

    added final working oracle contract v1.1 and relying contracts

--- RelyingContract1_v1.1.sol
+++ RelyingContract1_v1.1.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.25;
-import "https://github.com/georgejohnchavady/georacle/blob/master/OracleContractv1.0.sol";
-contract ExampleContract {
+import "https://github.com/georgejohnchavady/georacle/blob/master/OracleContractv1.1.sol";
+contract ExampleContract is Oracle{
 
     event LogNewOracleQuery(string description);
 
@@ -9,14 +9,14 @@ contract ExampleContract {
 
     function updatePrice() payable public{
         //send 12 wei while creating contract instance
-        address oracle_addr  = 0xA6DF3F81d2Ac0C773aC2dC8bb22b50973d5D0059;
+        address oracle_addr  = 0xcBFF55037620fB1797ea4Da6a293947387267e74;
         Oracle oracle = Oracle(oracle_addr);
         string memory url = "https://api.pro.coinbase.com/products/ETH-USD/ticker";
         string memory attribute = "price";
-        oracle.createRequest(false, url, attribute);
+        oracle.createRequest(true, url, attribute);
     }
 
-    function __callback(uint id, string result) payable public{
+    function callback(uint id, string result) payable public{
         emit LogNewOracleResponse(id, result);
     }
 }
--- RelyingContract2_v1.1.sol
+++ RelyingContract2_v1.1.sol
@@ -1,25 +1,22 @@
 pragma solidity ^0.4.25;
-import "https://github.com/georgejohnchavady/georacle/blob/master/OracleContractv1.0.sol";
-contract ExampleContract {
+import "https://github.com/georgejohnchavady/georacle/blob/master/OracleContractv1.1.sol";
+contract ExampleContract is Oracle{
 
     event LogNewOracleQuery(string description);
-    event LogNewOracleResponse(string response);
-    event LogId(uint id);
 
+    event LogNewOracleResponse(uint id, string response);
 
 
     function updatePrice() payable public{
         //send 12 wei while creating contract instance
-        address oracle_addr  = 0x189ef95f5e90c05e79f1be12b111108c96920389;
+        address oracle_addr  = 0xcBFF55037620fB1797ea4Da6a293947387267e74;
         Oracle oracle = Oracle(oracle_addr);
         string memory url = "https://api.pro.coinbase.com/products/ETH-USD/ticker";
         string memory attribute = "price";
-        oracle.createRequest(true, url, attribute);
-        emit LogNewOracleQuery("request triggered");
+        oracle.createRequest(false, url, attribute);
     }
 
-    function __callback(uint id, string result) payable public{
-        emit LogId(id);
-        emit LogNewOracleResponse(result);
+    function callback(uint id, string result) payable public{
+        emit LogNewOracleResponse(id, result);
     }
 }
